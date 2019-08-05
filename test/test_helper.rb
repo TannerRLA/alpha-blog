c99b056c81bb931e0c2426c6275df88c4b24d103
Tanner <TannerRLA@gmail.com>
2019-08-05 08:03:16 -0700

    add categories drop down to the navigation

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,9 +6,10 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  
+
+  # Add more helper methods to be used by all tests here...
   def sign_in_as(user,password)
   	post login_path, params:{ session: {email: user.email, password:password}}
   end
-
-  # Add more helper methods to be used by all tests here...
 end
